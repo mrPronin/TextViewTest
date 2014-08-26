de23553d6480cd4ba497cfb4bdf9bb9c28bfc70f
Alexander Pronin <pronin.alx@gmail.com>
2014-08-26 19:24:50 +0300

    Initial Commit

---
+++ TextViewTest/RITViewController.h
@@ -0,0 +1,13 @@
+//
+//  RITViewController.h
+//  TextViewTest
+//
+//  Created by Pronin Alexander on 26.08.14.
+//  Copyright (c) 2014 Pronin Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RITViewController : UIViewController
+
+@end
